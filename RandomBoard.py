8e7440262f8862bbfb06cc6f1e249a899d81bf1c
kelvin389 <44041554+kelvin389@users.noreply.github.com>
2020-05-07 12:11:36 -0700

    Add files via upload

---
+++ RandomBoard.py
@@ -0,0 +1,26 @@
+import random
+
+def print2DArray(array):
+	for i in range(len(array)):
+		for j in range(len(arr[0])):
+			print(array[i][j], " ", end="")
+		print()
+
+def findSum(array):
+	sum = 0
+
+	for i in range(len(array)):
+		for j in range(len(array[0])):
+			sum += array[i][j]
+	return sum
+
+
+
+arr = [[0 for x in range(10)] for y in range(10)]
+
+for i in range(10):
+	for j in range(10):
+		arr[i][j] = random.randint(0, 9)
+
+print2DArray(arr)
+print(findSum(arr))
